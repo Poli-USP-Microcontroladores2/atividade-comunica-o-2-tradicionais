8eee02bdf571198fe678e3ca49affa549bfa7997
Alexander Oliveira <alexander.kono.oliveira@gmail.com>
2025-11-13 14:58:40 -0300

    Codigo do echo bot para modificar

--- src/main.c
+++ src/main.c
@@ -1,135 +1,107 @@
 /*
- * Copyright (c) 2025 Embeint Inc
+ * Copyright (c) 2022 Libre Solar Technologies GmbH
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/net_buf.h>
-#include <zephyr/logging/log.h>
 #include <zephyr/drivers/uart.h>
-#include <zephyr/random/random.h>
+
+#include <string.h>
 
 /* change this to any other UART peripheral if desired */
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
 
-/* Maximum number of packets to generate per iteration */
-#define LOOP_ITER_MAX_TX 4
-/* Maximum size of our TX packets */
-#define MAX_TX_LEN 32
-#define RX_CHUNK_LEN 32
+#define MSG_SIZE 32
 
-/* Buffer pool for our TX payloads */
-NET_BUF_POOL_DEFINE(tx_pool, LOOP_ITER_MAX_TX, MAX_TX_LEN, 0, NULL);
-
-struct k_fifo tx_queue;
-struct net_buf *tx_pending_buffer;
-uint8_t async_rx_buffer[2][RX_CHUNK_LEN];
-volatile uint8_t async_rx_buffer_idx;
+/* queue to store up to 10 messages (aligned to 4-byte boundary) */
+K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
 
 static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
 
-LOG_MODULE_REGISTER(sample, LOG_LEVEL_INF);
+/* receive buffer used in UART ISR callback */
+static char rx_buf[MSG_SIZE];
+static int rx_buf_pos;
 
-static void uart_callback(const struct device *dev, struct uart_event *evt, void *user_data)
+/*
+ * Read characters from UART until line end is detected. Afterwards push the
+ * data to the message queue.
+ */
+void serial_cb(const struct device *dev, void *user_data)
 {
-	struct net_buf *buf;
-	int rc;
+	uint8_t c;
 
-	LOG_DBG("EVENT: %d", evt->type);
+	if (!uart_irq_update(uart_dev)) {
+		return;
+	}
 
-	switch (evt->type) {
-	case UART_TX_DONE:
-		LOG_DBG("TX complete %p", tx_pending_buffer);
+	if (!uart_irq_rx_ready(uart_dev)) {
+		return;
+	}
 
-		/* Free TX buffer */
-		net_buf_unref(tx_pending_buffer);
-		tx_pending_buffer = NULL;
+	/* read until FIFO empty */
+	while (uart_fifo_read(uart_dev, &c, 1) == 1) {
+		if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
+			/* terminate string */
+			rx_buf[rx_buf_pos] = '\0';
 
-		/* Handle any queued buffers */
-		buf = k_fifo_get(&tx_queue, K_NO_WAIT);
-		if (buf != NULL) {
-			rc = uart_tx(dev, buf->data, buf->len, 0);
-			if (rc != 0) {
-				LOG_ERR("TX from ISR failed (%d)", rc);
-				net_buf_unref(buf);
-			} else {
-				tx_pending_buffer = buf;
-			}
+			/* if queue is full, message is silently dropped */
+			k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
+
+			/* reset the buffer (it was copied to the msgq) */
+			rx_buf_pos = 0;
+		} else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
+			rx_buf[rx_buf_pos++] = c;
 		}
-		break;
-	case UART_RX_BUF_REQUEST:
-		/* Return the next buffer index */
-		LOG_DBG("Providing buffer index %d", async_rx_buffer_idx);
-		rc = uart_rx_buf_rsp(dev, async_rx_buffer[async_rx_buffer_idx],
-				     sizeof(async_rx_buffer[0]));
-		__ASSERT_NO_MSG(rc == 0);
-		async_rx_buffer_idx = async_rx_buffer_idx ? 0 : 1;
-		break;
-	case UART_RX_BUF_RELEASED:
-	case UART_RX_DISABLED:
-		break;
-	case UART_RX_RDY:
-		LOG_HEXDUMP_INF(evt->data.rx.buf + evt->data.rx.offset,
-				evt->data.rx.len, "RX_RDY");
-		break;
-	default:
-		LOG_WRN("Unhandled event %d", evt->type);
+		/* else: characters beyond buffer size are dropped */
+	}
+}
+
+/*
+ * Print a null-terminated string character by character to the UART interface
+ */
+void print_uart(char *buf)
+{
+	int msg_len = strlen(buf);
+
+	for (int i = 0; i < msg_len; i++) {
+		uart_poll_out(uart_dev, buf[i]);
 	}
 }
 
 int main(void)
 {
-	bool rx_enabled = false;
-	struct net_buf *tx_buf;
-	int loop_counter = 0;
-	uint8_t num_tx;
-	int tx_len;
-	int rc;
+	char tx_buf[MSG_SIZE];
 
-	/* Register the async interrupt handler */
-	uart_callback_set(uart_dev, uart_callback, (void *)uart_dev);
-
-	while (1) {
-		/* Wait a while until the next burst transmission */
-		k_sleep(K_SECONDS(5));
-
-		/* Each loop, try to send a random number of packets */
-		num_tx = (sys_rand32_get() % LOOP_ITER_MAX_TX) + 1;
-		LOG_INF("Loop %d: Sending %d packets", loop_counter, num_tx);
-		for (int i = 0; i < num_tx; i++) {
-			/* Allocate the data packet */
-			tx_buf = net_buf_alloc(&tx_pool, K_FOREVER);
-			/* Populate it with data */
-			tx_len = snprintk(tx_buf->data, net_buf_tailroom(tx_buf),
-					  "Loop %d: Packet: %d\r\n", loop_counter, i);
-			net_buf_add(tx_buf, tx_len);
-
-			/* Queue packet for transmission */
-			rc = uart_tx(uart_dev, tx_buf->data, tx_buf->len, SYS_FOREVER_US);
-			if (rc == 0) {
-				/* Store the pending buffer */
-				tx_pending_buffer = tx_buf;
-			} else if (rc == -EBUSY) {
-				/* Transmission is already in progress */
-				LOG_DBG("Queuing buffer %p", tx_buf);
-				k_fifo_put(&tx_queue, tx_buf);
-			} else {
-				LOG_ERR("Unknown error (%d)", rc);
-			}
-		}
-
-		/* Toggle the RX state */
-		if (rx_enabled) {
-			uart_rx_disable(uart_dev);
-		} else {
-			async_rx_buffer_idx = 1;
-			uart_rx_enable(uart_dev, async_rx_buffer[0], RX_CHUNK_LEN, 100);
-		}
-		rx_enabled = !rx_enabled;
-		LOG_INF("RX is now %s", rx_enabled ? "enabled" : "disabled");
-
-		loop_counter += 1;
+	if (!device_is_ready(uart_dev)) {
+		printk("UART device not found!");
+		return 0;
 	}
+
+	/* configure interrupt and callback to receive data */
+	int ret = uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
+
+	if (ret < 0) {
+		if (ret == -ENOTSUP) {
+			printk("Interrupt-driven UART API support not enabled\n");
+		} else if (ret == -ENOSYS) {
+			printk("UART device does not support interrupt-driven API\n");
+		} else {
+			printk("Error setting UART callback: %d\n", ret);
+		}
+		return 0;
+	}
+	uart_irq_rx_enable(uart_dev);
+
+	print_uart("Hello! I'm your echo bot.\r\n");
+	print_uart("Tell me something and press enter:\r\n");
+
+	/* indefinitely wait for input from the user */
+	while (k_msgq_get(&uart_msgq, &tx_buf, K_FOREVER) == 0) {
+		print_uart("Echo: ");
+		print_uart(tx_buf);
+		print_uart("\r\n");
+	}
+	return 0;
 }
